7ccd408ad343b448575ba53bb9538edd0ac9afa5
Yury Moshkin <yumoshkin@gmail.com>
2024-12-11 11:18:50 +0300

    Add task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Car> cars = new ArrayList<>();
+        Race race = new Race();
+
+        for (int i = 1; i <= 3; i++) {
+            cars.add(createCar(scanner, i));
+        }
+
+        System.out.println("Самая быстрая машина: " + race.getLeader(cars));
+    }
+
+    static Car createCar(Scanner scanner, int number) {
+        System.out.println("Введите название машины №" + number);
+        String name = scanner.next();
+
+        while (true) {
+            String speedError = "Неправильная скорость";
+
+            try {
+                System.out.println("Введите скорость машины №" + number);
+                int speed = scanner.nextInt();
+
+                if (speed < 0 || speed > 250) {
+                    System.out.println(speedError);
+                } else {
+                    return new Car(name, speed);
+                }
+            } catch (Exception e) {
+                System.out.println(speedError);
+                scanner.nextLine();
+            }
+        }
     }
 }
